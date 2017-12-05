c40d132ff5e35fcf5a103a923601734d89aff48e
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2017-12-05 17:05:54 +0100

    Switching Weapons & Projectile model

---
+++ ProjectP2/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+
+    public GameObject staff;
+    public GameObject book;
+    public bool switched;
+
+	// Use this for initialization
+	void Start () {
+        switched = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            switched = !switched;
+            if (switched == true)
+            {
+                staff.SetActive(false);
+                book.SetActive(true);
+            }
+            if(switched == false)
+            {
+                staff.SetActive(true);
+                book.SetActive(false);
+            }
+        }
+	}
+}
